64c9d40f057f4acc9b64f2583fe854e5569e4ed0
li-lauren <laurenlidev@gmail.com>
2021-01-13 14:15:05 -0700

    Add second set of data from Jun 2019 to end of July 2020

--- parse_data.py
+++ parse_data.py
@@ -1,35 +1,36 @@
 import json
 
-with open('data/chicago_public_urination.csv', 'r') as f:
-    lines = f.readlines()[1:]
+id = 1
+for csv_file in ['data/chicago_public_urination.csv', 'data/chicago_public_urination_2.csv']:
+    with open(csv_file, 'r') as f:
+        lines = f.readlines()[1:]
 
-    notice_data = {
-        "type": "FeatureCollection",
+        notice_data = {
+            "type": "FeatureCollection",
 
-        "features": []
-    }
+            "features": []
+        }
 
-    id = 1
-
-    for line in lines:
-        line_s = line.split(",")
-        notice_data['features'].append({
-            "type": "Feature", 
-            "id": id,
-            "properties": {
-                "NOV_DATE": line_s[0],
-                "NOV_NUM": line_s[1],
-                "STR_ADDRESS": line_s[2],
-                "DESC": line_s[5],
-                "LATITUDE": line_s[6],
-                "LONGITUDE": line_s[7]
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [line_s[7], line_s[6]]
-            }
-        })
-        id += 1
+        for line in lines:
+            line_s = line.split(",")
+            if 'UNKNOWN' not in line_s[2]:
+                notice_data['features'].append({
+                    "type": "Feature", 
+                    "id": id,
+                    "properties": {
+                        "NOV_DATE": line_s[0],
+                        "NOV_NUM": line_s[1],
+                        "STR_ADDRESS": line_s[2],
+                        "DESC": line_s[5],
+                        "LATITUDE": line_s[6],
+                        "LONGITUDE": line_s[7]
+                    },
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [line_s[7], line_s[6]]
+                    }
+                })
+                id += 1
 
 with open('data/chicago_public_urination.json', 'w') as outfile:
     json.dump(notice_data, outfile, indent=4)
